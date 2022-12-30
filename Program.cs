357b34fd8468448d55e6cee6594d4393456e6ef7
jastasovas <jastasovas@gmail.com>
2022-12-30 19:32:12 +0200

    Added last tasks

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ Department depp = new Department();
 //Department dep3 = new Department { Name = "Mathematical Sciences" };
 //Department dep4 = new Department { Name = "Physics" };
 
-//-------- 1 -----------
+//-------- Task 1 -----------
+
 //var lec1 = new Lecture {Name = "Informatics"};
 //var lec2 = new Lecture {Name = "Physics"};
 //var stud1 = new Student {FirstName = "Vardunas", LastName = "Pavardunas"};
@@ -21,7 +22,7 @@ Department depp = new Department();
 //context.AddRange(dep5);
 //context.SaveChanges();
 
-//-------- 2 -----------
+//-------- Task 2 -----------
 
 //var stud3 = new Student { FirstName = "Jonas", LastName = "Jonaitis" };
 //var stud4 = new Student {FirstName = "Janina", LastName = "Janinaite"};
@@ -36,7 +37,7 @@ Department depp = new Department();
 //context.Departments.Update(depar);
 //context.SaveChanges();
 
-//-------- 3 -----------
+//-------- Task 3 -----------
 
 //var lec5 = new Lecture { Name = "Biology" };
 //var depar = context.Departments.Where(x => x.Name == "Chemistry").Include(l => l.Lectures).First();
@@ -44,21 +45,43 @@ Department depp = new Department();
 //context.Departments.Update(depar);
 //context.SaveChanges();
 
-//-------- 4 -----------
+//-------- Task 4 -----------
+
 //var stud5 = new Student {FirstName = "Petras", LastName = "Petrauskas", DepartmentId = 2};
 //context.Students.Add(stud5);    
 //context.SaveChanges();  
 
-//-------- 5 -----------
+//-------- Task 5 -----------
 
 //var student = context.Students.First(x => x.Id == 2);
 //student.DepartmentId = 3;
 //context.SaveChanges();  
 
-//-------- 6 -----------
-//var studentsList = context.Students.Where(x => x.DepartmentId == 5).Select(y => y.FirstName).ToList();
+//-------- Task 6 -----------
+
+//var studentsList = context.Students.Where(x => x.DepartmentId == 2).Select(y => y.FirstName).ToList();
 //foreach (var student in studentsList)
 //{
 //    Console.WriteLine(student);
 //}
-//-------- 6 -----------
+
+//-------- Task 7 -----------
+
+//var lecturesList = context.Departments.Include(x => x.Lectures).Where(y => y.Id == 2).First();
+//var lectures = lecturesList.Lectures;
+
+//foreach (var lecture in lectures) 
+//{
+//    Console.WriteLine(lecture.Name);
+//}     
+
+//-------- Task 8 -----------
+
+//var lecturesList = context.Students.Where(x => x.Id == 1).Select(y => y.DepartmentId).First();
+//var lectures = context.Departments.Include(x => x.Lectures).Include(y => y.Students).Where(z => z.Id == lecturesList).First();
+//var finalLectures = lectures.Lectures;
+
+//foreach (var lecture in finalLectures)
+//{
+//    Console.WriteLine(lecture.Name);
+//}
